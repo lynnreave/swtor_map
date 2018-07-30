64b4cdf99f772bcc2f8db88d67e5839bfeee6559
Sephone Slattery <sephone@multiscalehn.com>
2018-07-29 17:03:00 -0700

    Initial commit. Ported star wars galaxy from existing mod as baseline.

---
+++ common/defines/SWGD_defines.lua
@@ -0,0 +1,6 @@
+--Changes height of random spawns
+NDefines.NGraphics.RANDOM_HEIGHT_MIN = -3
+NDefines.NGraphics.RANDOM_HEIGHT_MAX = 3
+NDefines.NGameplay.BORDER_BASE_RADIUS= 6	-- The base radius of the border
+NDefines.NGameplay.BORDER_NEW_COLONY_START= 0.25		-- Newly started colony's borders is modified by this, goes up to 1x with progress
+NDefines.NGameplay.BORDER_POPULATION_MODIFIER= 0.4		-- The border population modifier
